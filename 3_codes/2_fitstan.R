1283e8da0930ded5c63e1adad8094cf3a4f07a8c
tkucecon <takeuchi.takanori.44c@gmail.com>
2023-04-05 14:23:29 -0400

    organized the folder structure

--- 3_codes/2_fitstan.R
+++ 3_codes/2_fitstan.R
@@ -1,7 +1,7 @@
 
 # ------------------------------------------------------------------------------
 # About this code
-# Estimate dynamic Bayesian logistic regression
+# Fit Stan code and conduct MCMC to estimate the main regressions
 # ------------------------------------------------------------------------------
 
 # set up------------------------------------------------------------------------
@@ -12,7 +12,6 @@
   theme_set(theme_solarized())
   library("patchwork")
   
-  library("tidymodels")
   library("rstan")
   
   # some functions
@@ -30,63 +29,49 @@
 # ------------------------------------------------------------------------------
   
   # load the JST data
-  load("../4_data/df_JST.rda")
-  
-  # create a recipe to impute the variables
-  recipe.bag <- 
-    recipe(x = df.JST, formula = as.formula(crisis ~ .)) %>% 
-    # remove year and country from the predictors %>% 
-    update_role(year,    new_role = "time variable") %>% 
-    update_role(country, new_role = "id variable") %>% 
-    # normalize all the predictors to be distributed ~ N(0,1) 
-    step_normalize(all_predictors(), skip = FALSE) %>% 
-    # delete rows with no variance
-    step_zv(all_predictors(), skip = FALSE) 
-  
-  # create a data frame with imputed variables
-  df.JST <- 
-    recipe.bag %>% 
-    prep() %>% 
-    bake(new_data = df.JST)
+  load("../4_data/df_JST_normalized.rda")
   
   # keep only the relevant variables and remove NA rows
-  df.JST <- 
-    df.JST %>% 
+  df.JST.normalized <- 
+    df.JST.normalized %>% 
     select(year, country, crisis, 
-           starts_with("credit"),
-           starts_with("slope"),
-           starts_with("pi"),
-           starts_with("money"),
-           starts_with("equity"),
-           starts_with("iy"),
-           rcon.dom,
-           ca.dom,
-           dsr.dom,
-           hpreal.dom,
-           starts_with("lev")) %>% 
+           # macro variables
+           starts_with("growth.gdpreal"),
+           starts_with("diff.credit"),
+           starts_with("level.slope"),
+           starts_with("growth.cpi"),
+           starts_with("diff.money"),
+           starts_with("growth.equity"),
+           starts_with("diff.iy"),
+           diff.ca.dom,
+           diff.dsr.dom,
+           growth.hpreal.dom,
+           # bank balance sheet variables
+           starts_with("level.lev")
+           ) %>% 
     na.omit()
 
 # ------------------------------------------------------------------------------
-# Run MCMC: dynamic logistic regression
+# Prepare the data set
 # ------------------------------------------------------------------------------
   
   # create the country ID according to countries
-  df.JST <- 
-    df.JST %>% 
+  df.JST.normalized <- 
+    df.JST.normalized %>% 
     group_by(year) %>% 
     mutate(time.id = cur_group_id()) %>% 
     ungroup()
   
   # create a data frame indicating year and ID 
   df.time.id <- 
-    df.JST %>% 
+    df.JST.normalized %>% 
     select(year, time.id) %>% 
     unique() %>% 
     arrange(time.id)
   
   # matrix of explanatory variables
   X.JST <- 
-    df.JST %>% 
+    df.JST.normalized %>% 
     select(-year, -country, -crisis, -time.id) %>% 
     as.matrix()
   
@@ -95,7 +80,7 @@
   
   # vector of response dummy variable
   Y <- 
-    df.JST %>% 
+    df.JST.normalized %>% 
     select(crisis) %>% 
     mutate(crisis = as.numeric(crisis) - 1) %>% 
     unlist() %>% 
@@ -103,7 +88,7 @@
   
   # vector of time id
   Tid <- 
-    df.JST$time.id
+    df.JST.normalized$time.id
   
   # input data
   data.stan <- 
@@ -115,9 +100,13 @@
          Tid      = Tid
     )
   
+# ------------------------------------------------------------------------------
+# Baseline: Gaussian state equation
+# ------------------------------------------------------------------------------
+  
   # fit the Bayesian model
   fit.stan <- 
-    stan(file   = "5_dynamic.stan",
+    stan(file   = "stan/gaussian.stan",
          data   = data.stan,
          seed   = 2292,
          pars   = c("beta", "theta"),
@@ -125,14 +114,11 @@
          iter   = 3000)
   
   # This process takes some time... save the result in the tmp folder
-  save(fit.stan, file = "../5_tmp/fit_dynamic.rda")
+  save(fit.stan, file = "../5_tmp/fit_gaussian.rda")
   
-# ------------------------------------------------------------------------------
-# Check the posteriors of the MCMC
-# ------------------------------------------------------------------------------
-
+  # Check the posteriors of the MCMC
   # If you start from this line, run this code
-  # load("../5_tmp/fit_dynamic.rda")
+  load("../5_tmp/fit_gaussian.rda")
   
   # extract the fitted model
   extracted.stan <- 
@@ -155,5 +141,45 @@
          width = 10, height = 4, 
          filename = "../6_outputs/ts_gaussian.pdf")
   
+# ------------------------------------------------------------------------------
+# Robustness check: structural change
+# ------------------------------------------------------------------------------
+  
+  # fit the Bayesian model
+  fit.stan <- 
+    stan(file   = "stan/structural_change.stan",
+         data   = data.stan,
+         seed   = 2292,
+         pars   = c("beta"),
+         warmup = 2000,
+         iter   = 3000)
+  
+  # This process takes some time... save the result in the tmp folder
+  save(fit.stan, file = "../5_tmp/fit_structural_change.rda")
+  
+  # Check the posteriors of the MCMC
+  # If you start from this line, run this code
+  # load("../5_tmp/fit_structural_change.rda")
+  
+  # extract the fitted model
+  extracted.stan <- 
+    extract(fit.stan)
+  
+  # plot the heat-map
+  g.heat <- 
+    plot.heat(significance = FALSE)
+  
+  # plot only significant variables
+  g.sig.ts <- 
+    plot.dynamic()
+  
+  # save the graph
+  ggsave(plot = g.heat, 
+         width = 10, height = 4, 
+         filename = "../6_outputs/heat_sc.pdf")
+  
+  ggsave(plot = g.sig.ts, 
+         width = 10, height = 4, 
+         filename = "../6_outputs/ts_sc.pdf")
   
   
