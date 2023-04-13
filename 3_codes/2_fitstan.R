9cd3fdea9799e62b8b227089ce026db812d92890
tkucecon <takeuchi.takanori.44c@gmail.com>
2023-04-12 23:49:58 -0400

    added NGG prior for robustness check

--- 3_codes/2_fitstan.R
+++ 3_codes/2_fitstan.R
@@ -44,104 +44,30 @@
            starts_with("growth.cpi"),
            starts_with("diff.money"),
            starts_with("growth.equity"),
+           starts_with("growth.hpreal"),
            starts_with("diff.iy"),
            diff.ca.dom,
            diff.dsr.dom,
-           growth.hpreal.dom,
            # bank balance sheet variables
            starts_with("level.lev")
            ) %>% 
-    na.omit()
-  
-  # data frame 2: bank equity return (f1) with baseline explanatory variables
-  df.eqf1.baseline <- 
-    df.JST.normalized %>% 
-    select(year, country, bank.eq.f1, 
-           # macro variables
-           starts_with("diff.credit"),
-           starts_with("level.slope"),
-           starts_with("growth.cpi"),
-           starts_with("diff.money"),
-           starts_with("growth.equity"),
-           starts_with("diff.iy"),
-           diff.ca.dom,
-           diff.dsr.dom,
-           growth.hpreal.dom,
-           # bank balance sheet variables
-           starts_with("level.lev")
-    ) %>% 
-    na.omit()
-
-  # data frame 3: crisis dummy with all explanatory variables (excluding gap vars)
-  df.crisis.allvars <- 
-    df.JST.normalized %>% 
-    select(year, country, crisis, 
-           # macro variables
-           starts_with("level."),
-           starts_with("diff."),
-           starts_with("growth.")
-    ) %>% 
-    na.omit()
-  
-  # data frame 4: bank equity return (f1) with all explanatory variables (excluding gap vars)
-  df.eqf1.allvars <- 
-    df.JST.normalized %>% 
-    select(year, country, bank.eq.f1, 
-           # macro variables
-           starts_with("level."),
-           starts_with("diff."),
-           starts_with("growth.")
-    ) %>% 
-    na.omit()
-  
-  # data frame 5: crisis dummy with only relevant (core) explanatory variables
-  df.crisis.core <- 
-    df.JST.normalized %>% 
-    select(year, country, crisis, 
-           diff.credit.dom,
-           level.slope.dom,
-           diff.money.glo,
-           diff.iy.dom,
-           diff.iy.glo,
-           diff.ca.dom,
-           diff.dsr.dom,
-           level.lev.dom
-    ) %>% 
     na.omit()
   
 # ------------------------------------------------------------------------------
 # run MCMC in multiple settings and save under 5_tmp folder
 # ------------------------------------------------------------------------------
   
-  # 1. gaussian transition with df.crisis.baseline
+  # 1. gaussian transition with df.crisis.baseline: horseshoe prior
   saveMCMC(df        = df.crisis.baseline,
            target    = "crisis",
            stan.file = "gaussian.stan",
            MCMC.name = "gaussian_crisis_baseline.rda")
-  
-  # 2. gaussian transition with df.equity.baseline
-  saveMCMC(df        = df.eqf1.baseline,
-           target    = "bank.eq.f1",
-           stan.file = "lmgaussian.stan",
-           MCMC.name = "lmgaussian_eqf1_baseline.rda")
-  
-  # 3. gaussian transition with df.crisis.allvars
-  saveMCMC(df        = df.crisis.allvars,
+
+  # 2. gaussian transition with df.crisis.baseline: NGG prior
+  saveMCMC(df        = df.crisis.baseline,
            target    = "crisis",
-           stan.file = "gaussian.stan",
-           MCMC.name = "gaussian_crisis_allvars.rda")
-  
-  # 4. gaussian transition with df.equity.allvars
-  saveMCMC(df        = df.eqf1.allvars,
-           target    = "bank.eq.f1",
-           stan.file = "lmgaussian.stan",
-           MCMC.name = "lmgaussian_eqf1_allvars.rda")
-  
-  # 5. possible structural breaks with df.crisis.baseline
-  # saveMCMC(df        = df.crisis.baseline,
-  #          target    = "crisis",
-  #          stan.file = "koop.stan",
-  #          MCMC.name = "koop_crisis_baseline.rda")
+           stan.file = "normal-gamma-gamma.stan",
+           MCMC.name = "NGG_crisis_baseline.rda")
   
 # ------------------------------------------------------------------------------
 # plot the results
@@ -154,25 +80,10 @@
   plot.dynamic(MCMC.name  = "gaussian_crisis_baseline.rda",
                graph.name = "gaussian_crisis_baseline_ts.pdf")
 
-  # 2. gaussian transition with df.equity.baseline
-  plot.heat(MCMC.name  = "lmgaussian_eqf1_baseline.rda",
-            graph.name = "lmgaussian_eqf1_baseline_heat.pdf")
+  # 2. gaussian transition with df.crisis.baseline: NGG prior
+  plot.heat(MCMC.name  = "NGG_crisis_baseline.rda",
+            graph.name = "NGG_crisis_baseline_heat.pdf")
   
-  plot.dynamic(MCMC.name  = "lmgaussian_eqf1_baseline.rda",
-               graph.name = "lmgaussian_eqf1_baseline_ts.pdf")
+  plot.dynamic(MCMC.name  = "NGG_crisis_baseline.rda",
+               graph.name = "NGG_crisis_baseline_ts.pdf")
   
-  # 3. gaussian transition with df.crisis.allvars
-  plot.heat(MCMC.name  = "gaussian_crisis_allvars.rda",
-            graph.name = "gaussian_crisis_allvars_heat.pdf")
-  
-  plot.dynamic(MCMC.name  = "gaussian_crisis_allvars.rda",
-               graph.name = "gaussian_crisis_allvars_ts.pdf")
-  
-  # 4. gaussian transition with df.equity.baseline
-  plot.heat(MCMC.name  = "lmgaussian_eqf1_allvars.rda",
-            graph.name = "lmgaussian_eqf1_allvars_heat.pdf")
-  
-  plot.dynamic(MCMC.name  = "lmgaussian_eqf1_allvars.rda",
-               graph.name = "lmgaussian_eqf1_allvars_ts.pdf")
-  
-  # 5. possible structural breaks with df.equity.core
---
+++ 3_codes/stan/normal-gamma-gamma.stan
@@ -0,0 +1,63 @@
+
+// -----------------------------------------------------------------------------
+// About this code
+// Estimate a logit model with time-varying coefficients
+// assumption of normal-gamma-gamma prior on beta_0 and theta
+// -----------------------------------------------------------------------------
+
+// input data
+data {
+  int                      I;      // sample size
+  int                      p;      // number of covariates
+  int                      Tmax;   // number of time variables
+  matrix[I, p]             X;      // explanatory variable matrix
+  int<lower=0, upper=1>    Y[I];   // crisis dummy
+  int<lower=1, upper=Tmax> Tid[I]; // time index of each sample
+}
+
+// parameters accepted by the model
+parameters {
+  // main
+  matrix[p, Tmax] beta;     // matrix of coefficients including intercept
+  vector<lower=0>[p] theta; // vector of sd of coefficients (controls smoothness)
+  // latent parameters for NGG prior: theta
+  vector<lower=0>[p] xi;
+  vector<lower=0>[p] kappa;
+  // latent parameters for NGG prior: beta
+  vector<lower=0>[p] tau;
+  vector<lower=0>[p] lambda;
+}
+
+// transformed parameters
+transformed parameters {
+  vector[I] q;    // main equation of logistic regression
+  for (i in 1:I) {
+    q[i] = inv_logit(X[i] * beta[, Tid[i]]);
+  }
+  // hyperparameters for NGG: theta
+  real<lower=0> a_xi    = 0.1;
+  real<lower=0> c_xi    = 0.1;
+  real<lower=0> kappa_b = 50;
+  // hyperparameters for NGG: the initial value of beta
+  real<lower=0> a_tau = 0.5;
+  real<lower=0> c_tau = 0.5;
+  real<lower=0> lambda_b = 10;
+}
+
+// model part
+model {
+  // priors for theta: NGG prior
+  theta        ~ normal(0, xi);
+  xi           ~ gamma(a_xi, (a_xi * kappa / 2));
+  kappa        ~ gamma(c_xi, (c_xi / kappa_b));
+  // priors for the initial value of beta: NGG prior
+  beta[, 1]    ~ normal(0, tau);
+  tau          ~ gamma(a_tau, (a_tau * lambda / 2));
+  lambda       ~ gamma(c_tau, (c_tau / lambda_b));
+  // state equation: let coefficients evolve over time
+  for (t in 2:Tmax) {
+    beta[, t] ~ normal(beta[, t-1], theta);
+  }
+  // logistic regression likelihood
+  Y ~ bernoulli(q); // logistic regression
+}
