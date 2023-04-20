6d9283a9e825fbd5895372ef7dbf0b45b613e625
tkucecon <takeuchi.takanori.44c@gmail.com>
2023-04-20 13:53:46 -0400

    consistent with regdf

--- 3_codes/4_fitstan.R
+++ 3_codes/4_fitstan.R
@@ -12,11 +12,12 @@
   theme_set(theme_solarized())
   library("patchwork")
   
+  library("tidymodels")
   library("rstan")
   
   # some functions
+  source("util/regdf.r")
   source("util/saveNUTS.r")
-  source("util/saveADVI.r")
   source("util/plot_dynamic.r")
   source("util/plot_heat.r")
   source("util/plot_density.r")
@@ -31,51 +32,34 @@
 # Load the normalized data
 # ------------------------------------------------------------------------------
   
-  # load the normalized data
-  load("../4_data/df_normalized.rda")
+  # load the core data
+  load("../4_data/df_core.rda")
   
 # ------------------------------------------------------------------------------
-# run MCMC in multiple settings and save under 5_tmp folder
+# run MCMC in multiple settings and save under 5_tmp folder: for JST crisis 
 # ------------------------------------------------------------------------------
   
   # 1. horseshoe prior
-  saveNUTS(df        = df.normalized,
+  saveNUTS(df        = df.core,
+           target    = "JST",
            stan.file = "horseshoe")
 
-  # 2. Normal-Gamma-Gamma prior with estimated hyperparameter
-  saveNUTS(df        = df.normalized,
-           stan.file = "NGG")
-
-  # 3. Normal-Gamma-Gamma prior with given hyperparameter
-  hyperparams <- 
-    list(a_xi     = 0.1,
-         c_xi     = 0.5,
-         kappa_b  = 100,
-         a_tau    = 0.5,
-         c_tau    = 0.5,
-         lambda_b = 50
-         )
-  
-  saveNUTS(df          = df.normalized,
-           stan.file   = "NGG_manual",
-           hyperparams = hyperparams)
+  # 4. regularized horseshoe prior
+  saveNUTS(df        = df.core,
+           target    = "JST",
+           stan.file = "reg_horseshoe",
+           p0        = 4)
 
 # ------------------------------------------------------------------------------
 # plot the results
 # ------------------------------------------------------------------------------
 
   # 1. horseshoe prior
-  plot.heat("horseshoe")
-  plot.dynamic("horseshoe")
-  plot.density("horseshoe", type = 2) 
+  plot.heat("horseshoe_JST")
+  plot.dynamic("horseshoe_JST")
+
+  # regularized horseshoe prior
+  plot.heat("reg_horseshoe_JST")
+  plot.dynamic("reg_horseshoe_JST")
+
   
-  # 2. NGG prior with estimated hyper-parameters
-  plot.heat("NGG")
-  plot.dynamic("NGG")
-  plot.density("NGG", type = 2) 
-  plot.density("NGG", type = 3)  
-  
-  # 3. NGG prior with given hyper-parameters
-  plot.heat("NGG_manual")
-  plot.dynamic("NGG_manual")
-  plot.density("NGG_manual", type = 2) 
